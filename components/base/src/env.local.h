78b68e974e8b031b5384cd1752222078cfb94346
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2023-02-15 23:19:17 +0100

    adding files required by pipelines

---
+++ components/base/src/env.local.h
@@ -0,0 +1,24 @@
+/**
+ * \file evn.local.h
+ * \author T_IOT_901_STG_1_MSC_2023 (m.alognikou, d.kalumvuati, r.nguemo, r.mers and a.azzoug)
+ * \brief Header that contain environment variable
+ * \date Dec. 2022
+ * \copyright This code is under the MIT license but administrated by EPITECH Organization Github Workspace
+ */
+
+
+/**********************************************************************************************************************/
+/* DEFINITIONS                                                                                                        */
+/**********************************************************************************************************************/
+
+
+#define API_URL "http://161.35.208.61/api/index.php"
+#define DOLAPIKEY "DOLAPIKEY"
+#define API_USER "dolibarr"
+#define API_PASSWORD "IOT-API-DOLIBARR-ADMIN"
+
+
+
+/**********************************************************************************************************************/
+/* END OF FILE                                                                                                        */
+/**********************************************************************************************************************/
